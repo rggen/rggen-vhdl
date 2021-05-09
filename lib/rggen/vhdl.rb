3a9be8fdcf074b5adcdfb2399a6d590ddc9cb3b1
Taichi Ishitani <taichi730@gmail.com>
2021-05-09 11:43:23 +0900

    add rs, w0s, w1s, ws and wos bit field types

--- lib/rggen/vhdl.rb
+++ lib/rggen/vhdl.rb
@@ -25,6 +25,7 @@ module RgGen
         'vhdl/bit_field/type/rc_w0c_w1c_wc_woc',
         'vhdl/bit_field/type/ro',
         'vhdl/bit_field/type/rof',
+        'vhdl/bit_field/type/rs_w0s_w1s_ws_wos',
         'vhdl/bit_field/type/rw_w1_wo_wo1',
         'vhdl/bit_field/vhdl_top',
         'vhdl/register/vhdl_top',
---
+++ lib/rggen/vhdl/bit_field/type/rs_w0s_w1s_ws_wos.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RgGen.define_list_item_feature(:bit_field, :type, [:rs, :w0s, :w1s, :ws, :wos]) do
+  vhdl do
+    build do
+      input :clear, {
+        name: "i_#{full_name}_clear", width: width, array_size: array_size
+      }
+      output :value_out, {
+        name: "o_#{full_name}", width: width, array_size: array_size
+      }
+    end
+
+    main_code :bit_field, from_template: true
+
+    private
+
+    def read_action
+      {
+        rs: 'RGGEN_READ_SET',
+        w0s: 'RGGEN_READ_DEFAULT',
+        w1s: 'RGGEN_READ_DEFAULT',
+        ws: 'RGGEN_READ_DEFAULT',
+        wos: 'RGGEN_READ_NONE'
+      }[bit_field.type]
+    end
+
+    def write_action
+      {
+        rs: 'RGGEN_WRITE_NONE',
+        w0s: 'RGGEN_WRITE_0_SET',
+        w1s: 'RGGEN_WRITE_1_SET',
+        ws: 'RGGEN_WRITE_SET',
+        wos: 'RGGEN_WRITE_SET'
+      }[bit_field.type]
+    end
+
+    def write_enable
+      bit_field.writable? && bin(1) || bin(0)
+    end
+  end
+end
