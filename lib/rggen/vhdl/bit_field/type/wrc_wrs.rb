6fd63cc74e574553feeff99f25cd804c521e8cbe
Taichi Ishitani <taichi730@gmail.com>
2021-05-14 21:50:25 +0900

    add wrc and wrs bit field types

---
+++ lib/rggen/vhdl/bit_field/type/wrc_wrs.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RgGen.define_list_item_feature(:bit_field, :type, [:wrc, :wrs]) do
+  vhdl do
+    build do
+      output :value_out, {
+        name: "o_#{full_name}", width: width, array_size: array_size
+      }
+    end
+
+    main_code :bit_field, from_template: true
+
+    private
+
+    def read_action
+      {
+        wrc: 'RGGEN_READ_CLEAR',
+        wrs: 'RGGEN_READ_SET'
+      }[bit_field.type]
+    end
+  end
+end
---
+++ spec/rggen/vhdl/bit_field/type/wrs_spec.rb
@@ -0,0 +1,374 @@
+# frozen_string_literal: true
+
+RSpec.describe 'bit_field/type/wrs' do
+  include_context 'clean-up builder'
+  include_context 'vhdl common'
+
+  before(:all) do
+    RgGen.enable(:global, [:bus_width, :address_width, :array_port_format])
+    RgGen.enable(:register_block, :byte_size)
+    RgGen.enable(:register_file, [:name, :size, :offset_address])
+    RgGen.enable(:register, [:name, :size, :type, :offset_address])
+    RgGen.enable(:bit_field, [:name, :bit_assignment, :initial_value, :reference, :type])
+    RgGen.enable(:bit_field, :type, [:wrs])
+    RgGen.enable(:register_block, :vhdl_top)
+    RgGen.enable(:register_file, :vhdl_top)
+    RgGen.enable(:register, :vhdl_top)
+    RgGen.enable(:bit_field, :vhdl_top)
+  end
+
+  def create_bit_fields(&body)
+    create_vhdl(&body).bit_fields
+  end
+
+  it '出力ポート#value_outを持つ' do
+    bit_fields = create_bit_fields do
+      byte_size 256
+
+      register do
+        name 'register_0'
+        bit_field { name 'bit_field_0'; bit_assignment lsb: 0; type :wrs; initial_value 0 }
+        bit_field { name 'bit_field_1'; bit_assignment lsb: 8, width: 2; type :wrs; initial_value 0 }
+        bit_field { name 'bit_field_2'; bit_assignment lsb: 16, width: 4, sequence_size: 2, step: 8; type :wrs; initial_value 0 }
+      end
+
+      register do
+        name 'register_1'
+        bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 64; type :wrs; initial_value 0 }
+      end
+
+      register do
+        name 'register_2'
+        size [4]
+        bit_field { name 'bit_field_0'; bit_assignment lsb: 0; type :wrs; initial_value 0 }
+        bit_field { name 'bit_field_1'; bit_assignment lsb: 8, width: 2; type :wrs; initial_value 0 }
+        bit_field { name 'bit_field_2'; bit_assignment lsb: 16, width: 4, sequence_size: 2, step: 8; type :wrs; initial_value 0 }
+      end
+
+      register do
+        name 'register_3'
+        size [2, 2]
+        bit_field { name 'bit_field_0'; bit_assignment lsb: 0; type :wrs; initial_value 0 }
+        bit_field { name 'bit_field_1'; bit_assignment lsb: 8, width: 2; type :wrs; initial_value 0 }
+        bit_field { name 'bit_field_2'; bit_assignment lsb: 16, width: 4, sequence_size: 2, step: 8; type :wrs; initial_value 0 }
+      end
+
+      register_file do
+        name 'register_file_4'
+        size [2, 2]
+        register_file do
+          name 'register_file_0'
+          register do
+            name 'register_0'
+            size [2, 2]
+            bit_field { name 'bit_field_0'; bit_assignment lsb: 0; type :wrs; initial_value 0 }
+            bit_field { name 'bit_field_1'; bit_assignment lsb: 8, width: 2; type :wrs; initial_value 0 }
+            bit_field { name 'bit_field_2'; bit_assignment lsb: 16, width: 4, sequence_size: 2, step: 8; type :wrs; initial_value 0 }
+          end
+        end
+      end
+    end
+
+    expect(bit_fields[0]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_0_bit_field_0', direction: :out, width: 1
+    )
+    expect(bit_fields[1]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_0_bit_field_1', direction: :out, width: 2
+    )
+    expect(bit_fields[2]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_0_bit_field_2', direction: :out, width: 4, array_size: [2]
+    )
+
+    expect(bit_fields[3]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_1_bit_field_0', direction: :out, width: 64
+    )
+
+    expect(bit_fields[4]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_2_bit_field_0', direction: :out, width: 1, array_size: [4]
+    )
+    expect(bit_fields[5]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_2_bit_field_1', direction: :out, width: 2, array_size: [4]
+    )
+    expect(bit_fields[6]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_2_bit_field_2', direction: :out, width: 4, array_size: [4, 2]
+    )
+
+    expect(bit_fields[7]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_3_bit_field_0', direction: :out, width: 1, array_size: [2, 2]
+    )
+    expect(bit_fields[8]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_3_bit_field_1', direction: :out, width: 2, array_size: [2, 2]
+    )
+    expect(bit_fields[9]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_3_bit_field_2', direction: :out, width: 4, array_size: [2, 2, 2]
+    )
+
+    expect(bit_fields[10]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_file_4_register_file_0_register_0_bit_field_0', direction: :out, width: 1, array_size: [2, 2, 2, 2]
+    )
+    expect(bit_fields[11]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_file_4_register_file_0_register_0_bit_field_1', direction: :out, width: 2, array_size: [2, 2, 2, 2]
+    )
+    expect(bit_fields[12]).to have_port(
+      :register_block, :value_out,
+      name: 'o_register_file_4_register_file_0_register_0_bit_field_2', direction: :out, width: 4, array_size: [2, 2, 2, 2, 2]
+    )
+  end
+
+  describe '#generate_code' do
+    it 'rggen_bit_fieldをインスタンスするコードを出力する' do
+      bit_fields = create_bit_fields do
+        byte_size 256
+
+        register do
+          name 'register_0'
+          bit_field { name 'bit_field_0'; bit_assignment lsb: 0; type :wrs; initial_value 0 }
+          bit_field { name 'bit_field_1'; bit_assignment lsb: 16, width: 16; type :wrs; initial_value 0xabcd }
+        end
+
+        register do
+          name 'register_1'
+          bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 64; type :wrs; initial_value 0 }
+        end
+
+        register do
+          name 'register_2'
+          bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 4, sequence_size: 4, step: 8; type :wrs; initial_value 0 }
+        end
+
+        register do
+          name 'register_3'
+          size [4]
+          bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 4, sequence_size: 4, step: 8; type :wrs; initial_value 0 }
+        end
+
+        register do
+          name 'register_4'
+          size [2, 2]
+          bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 4, sequence_size: 4, step: 8; type :wrs; initial_value 0 }
+        end
+
+        register_file do
+          name 'register_file_5'
+          size [2, 2]
+          register_file do
+            name 'register_file_0'
+            register do
+              name 'register_0'
+              size [2, 2]
+              bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 4, sequence_size: 4, step: 8; type :wrs; initial_value 0 }
+            end
+          end
+        end
+      end
+
+      expect(bit_fields[0]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 1,
+            INITIAL_VALUE   => slice(x"0", 1, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(0 downto 0),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(0 downto 0),
+            i_sw_write_data   => bit_field_write_data(0 downto 0),
+            o_sw_read_data    => bit_field_read_data(0 downto 0),
+            o_sw_value        => bit_field_value(0 downto 0),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_0_bit_field_0,
+            o_value_unmasked  => open
+          );
+      CODE
+
+      expect(bit_fields[1]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 16,
+            INITIAL_VALUE   => slice(x"abcd", 16, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(31 downto 16),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(31 downto 16),
+            i_sw_write_data   => bit_field_write_data(31 downto 16),
+            o_sw_read_data    => bit_field_read_data(31 downto 16),
+            o_sw_value        => bit_field_value(31 downto 16),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_0_bit_field_1,
+            o_value_unmasked  => open
+          );
+      CODE
+
+      expect(bit_fields[2]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 64,
+            INITIAL_VALUE   => slice(x"0000000000000000", 64, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(63 downto 0),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(63 downto 0),
+            i_sw_write_data   => bit_field_write_data(63 downto 0),
+            o_sw_read_data    => bit_field_read_data(63 downto 0),
+            o_sw_value        => bit_field_value(63 downto 0),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_1_bit_field_0,
+            o_value_unmasked  => open
+          );
+      CODE
+
+      expect(bit_fields[3]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 4,
+            INITIAL_VALUE   => slice(x"0", 4, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(0+8*i+3 downto 0+8*i),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(0+8*i+3 downto 0+8*i),
+            i_sw_write_data   => bit_field_write_data(0+8*i+3 downto 0+8*i),
+            o_sw_read_data    => bit_field_read_data(0+8*i+3 downto 0+8*i),
+            o_sw_value        => bit_field_value(0+8*i+3 downto 0+8*i),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_2_bit_field_0(4*(i)+3 downto 4*(i)),
+            o_value_unmasked  => open
+          );
+      CODE
+
+      expect(bit_fields[4]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 4,
+            INITIAL_VALUE   => slice(x"0", 4, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(0+8*j+3 downto 0+8*j),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(0+8*j+3 downto 0+8*j),
+            i_sw_write_data   => bit_field_write_data(0+8*j+3 downto 0+8*j),
+            o_sw_read_data    => bit_field_read_data(0+8*j+3 downto 0+8*j),
+            o_sw_value        => bit_field_value(0+8*j+3 downto 0+8*j),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_3_bit_field_0(4*(4*i+j)+3 downto 4*(4*i+j)),
+            o_value_unmasked  => open
+          );
+      CODE
+
+      expect(bit_fields[5]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 4,
+            INITIAL_VALUE   => slice(x"0", 4, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(0+8*k+3 downto 0+8*k),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(0+8*k+3 downto 0+8*k),
+            i_sw_write_data   => bit_field_write_data(0+8*k+3 downto 0+8*k),
+            o_sw_read_data    => bit_field_read_data(0+8*k+3 downto 0+8*k),
+            o_sw_value        => bit_field_value(0+8*k+3 downto 0+8*k),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_4_bit_field_0(4*(8*i+4*j+k)+3 downto 4*(8*i+4*j+k)),
+            o_value_unmasked  => open
+          );
+      CODE
+
+      expect(bit_fields[6]).to generate_code(:bit_field, :top_down, <<~'CODE')
+        u_bit_field: entity work.rggen_bit_field
+          generic map (
+            WIDTH           => 4,
+            INITIAL_VALUE   => slice(x"0", 4, 0),
+            SW_READ_ACTION  => RGGEN_READ_SET
+          )
+          port map (
+            i_clk             => i_clk,
+            i_rst_n           => i_rst_n,
+            i_sw_valid        => bit_field_valid,
+            i_sw_read_mask    => bit_field_read_mask(0+8*m+3 downto 0+8*m),
+            i_sw_write_enable => "1",
+            i_sw_write_mask   => bit_field_write_mask(0+8*m+3 downto 0+8*m),
+            i_sw_write_data   => bit_field_write_data(0+8*m+3 downto 0+8*m),
+            o_sw_read_data    => bit_field_read_data(0+8*m+3 downto 0+8*m),
+            o_sw_value        => bit_field_value(0+8*m+3 downto 0+8*m),
+            i_hw_write_enable => "0",
+            i_hw_write_data   => (others => '0'),
+            i_hw_set          => (others => '0'),
+            i_hw_clear        => (others => '0'),
+            i_value           => (others => '0'),
+            i_mask            => (others => '1'),
+            o_value           => o_register_file_5_register_file_0_register_0_bit_field_0(4*(32*i+16*j+8*k+4*l+m)+3 downto 4*(32*i+16*j+8*k+4*l+m)),
+            o_value_unmasked  => open
+          );
+      CODE
+    end
+  end
+end
